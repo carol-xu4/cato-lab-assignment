4a1779e24a03b0b31a8fcbea849abe4bdee86cec
carol-xu4 <carol.xu@emory.edu>
2026-01-30 17:09:36 -0500

    data

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # Cato Institute Data Research Lab
 ### Carol Xu
 
+5 February 226
+
 ## Have middle-class incomes declined since the 1950s?
 
 ### Data sources:
 1. U.S. Census Bureau - Real Median Family Income in the United States (FRED)
-2. U.S. Bureau of Economicc Analysis - Real gross domestic product per capita (FRED)
+    - Units: 2023 CPI adjusted dollars
+2. U.S. Bureau of Economicc Analysis - Real gross domestic product per capita (FRED)    
+    - Units: 2017 Dollars
+
+Since the two datasets are adjusted by different measures, both income and GDP were indexed to compare relative growth over time, from 1953 to 2024. 
--- data-code/cleaning.r
+++ data-code/cleaning.r
@@ -5,9 +5,27 @@ pacman::p_load(tidyverse, ggplot2, dplyr, lubridate, stringr, readxl, data.table
 # Set working directory
 setwd("C:/Users/CarolXu/OneDrive - Cato Institute/Desktop/Cato Lab Assignment")
 
-## Read in data ------------------------------------------------------------
-dfwhc1 = read_excel("data/input/___.xlsx")
+# Read in data ------------------------------------------------------------
+realincome = read.csv("data/input/MEFAINUSA672N.csv")
+realgdp = read.csv("data/input/A939RX0Q048SBEA.csv")
 
-## Cleaning ----------------------------------------------------------------
+# cleaning ----------------------------------------------------------------
+realincome = realincome %>%
+    rename(year = observation_date,
+        income = MEFAINUSA672N)
 
-# rename columns
+realgdp = realgdp %>%
+    rename( year = observation_date,
+        gdp = A939RX0Q048SBEA)
+
+# merging columns by date, 1953 - 2024
+# NA rows were removed (february-december, and years < 1953), since income is annual and gdp was taken monthly
+data = merge(realincome, realgdp, 
+    by = "year", all = FALSE)
+
+# indexing percent growth since 1953, since columns adjusted by different units
+data = data %>%
+    mutate(gdp_index = gdp / gdp[year == "1953-01-01"] * 100,
+        income_index = income / income[year == "1953-01-01"] * 100)
+
+# writing final dataset ----------------------------------------------------

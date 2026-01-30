6364eb378d05bce5df280937109aec0fe8bf1091
carol-xu4 <carol.xu@emory.edu>
2026-01-30 13:52:25 -0500

    readme

--- data-code/cleaning.r
+++ data-code/cleaning.r
@@ -6,7 +6,7 @@ pacman::p_load(tidyverse, ggplot2, dplyr, lubridate, stringr, readxl, data.table
 setwd("C:/Users/CarolXu/OneDrive - Cato Institute/Desktop/Cato Lab Assignment")
 
 ## Read in data ------------------------------------------------------------
-dfwhc1 = read_excel("data/input/DFWHC1-26.xlsx")
+dfwhc1 = read_excel("data/input/___.xlsx")
 
 ## Cleaning ----------------------------------------------------------------
 

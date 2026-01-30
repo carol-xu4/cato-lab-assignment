6459cd9c2bb11bda9faa21fcbe8db3560cfbe8d6
carol-xu4 <carol.xu@emory.edu>
2026-01-30 13:17:10 -0500

    first commit

---
+++ data-code/cleaning.r
@@ -0,0 +1,13 @@
+## Preliminaries -----------------------------------------------------------
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyverse, ggplot2, dplyr, lubridate, stringr, readxl, data.table, gdata, readr)
+
+# Set working directory
+setwd("C:/Users/CarolXu/OneDrive - Cato Institute/Desktop/Cato Lab Assignment")
+
+## Read in data ------------------------------------------------------------
+dfwhc1 = read_excel("data/input/DFWHC1-26.xlsx")
+
+## Cleaning ----------------------------------------------------------------
+
+# rename columns
